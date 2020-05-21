424e667bc1312182017becd6e4ce81b849ec9233
irlyk <irlyk-on@mail.ru>
2020-05-21 18:09:39 +0300

    My type update

--- src/TValue.java
+++ src/TValue.java
@@ -1,19 +1,54 @@
 public class TValue {
 
     private enum Type {
-        INT, FLOAT, CHAR, BOOLEAN
+        INT, FLOAT, CHAR, BOOLEAN, NULL
     }
 
     private String varName;
     private Type type;
+    private String value;
 
+    private boolean isInteger(String str){
+        for (int i = 0; i < str.length(); i++){
+            if (!Character.isDigit(str.charAt(i)))
+                return false;
+        }
+        return true;
+    }
 
+    private boolean isFloat (String value){
+        boolean dotFalg = false;
+        if (!Character.isDigit(value.charAt(0))) return false;
+        for (int i = 1; i < value.length(); i++){
+            if (!Character.isDigit(value.charAt(i))) {
+                if (!dotFalg && value.charAt(i) == '.')
+                    dotFalg = true;
+                 else return false;
+            }
+        }
+        return true;
+    }
 
-    private Object value;
+    private boolean isBool(String value) {
+        return value.equals("true") || value.equals("false");
+    }
 
+    private boolean isChar (String value) {
+        return value.length() == 1;
+    }
 
-    public TValue(String varName){
-        this.varName = varName;
+    public TValue (String value) {
+        if (value == null || value.equals("")) this.value = null;
+        else if (isInteger(value)){
+            this.type = makeType("int");
+        } else if (isFloat(value)){
+            this.type = makeType("float");
+        } else if (isBool(value)){
+            this.type = makeType("bool");
+        } else if (isChar(value)) {
+            this.type = makeType("char");
+        } else this.type = Type.NULL;
+        this.value = value;
     }
 
     private Type makeType(String type){
@@ -36,7 +71,7 @@ public class TValue {
     }
 
 
-    public TValue(String varName, String type, Object value){
+    public TValue(String varName, String type, String value){
         this.varName = varName;
         this.type = makeType(type);
         this.value = value;
@@ -50,23 +85,38 @@ public class TValue {
         return type;
     }
 
-    public Object getValue() {
-        switch (type){
-            case INT:
-                return new Integer(value.toString());
-            case FLOAT:
-                return new Float(value.toString());
-            case CHAR:
-                return new Character(value.toString().charAt(0));
-            case BOOLEAN:
-                return new Boolean(value.toString());
-        }
-        return null;
+    public String getValue() {
+        return value;
     }
 
-    public void setValue(Object value) {
+    public void setVarName(String varName) {
+        this.varName = varName;
+    }
+
+    public void setValue(String value) {
         this.value = value;
     }
+
+    public int getInt() {
+        return Integer.parseInt(value);
+    }
+
+    public float getFloat() {
+        return Float.parseFloat(value);
+    }
+
+    public char getChar() {
+        return value.charAt(0);
+    }
+
+    public boolean getBool() {
+        return value.equals("true");
+    }
+
+    public Boolean isInt(){ return type == Type.INT; }
+    public Boolean isFloat(){ return type == Type.FLOAT; }
+    public Boolean isChar(){ return type == Type.CHAR; }
+    public Boolean isBool(){ return type == Type.BOOLEAN; }
 
     @Override
     public String toString() {
