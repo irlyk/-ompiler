cc407dc83ab9dd99c002469d8229c213932aa250
irlyk <irlyk-on@mail.ru>
2020-06-03 07:49:48 +0300

    Main update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,12 @@
+package main;
+
+import CodeGen.ast.Stmt;
 import MLL.MLLLexer;
 import MLL.MLLParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import visitor.MyVisitor;
 
 import javax.imageio.IIOException;
 import java.io.File;
@@ -14,9 +18,13 @@ public class Main {
 
     public static void main(String[] args)  {
         try {
+            if (args.length < 1) {
+                System.out.println("input file path required!");
+                return;
+            }
+            String inputFile = args[0];
             ANTLRInputStream input = new ANTLRInputStream(
-                    //new FileInputStream(args[0])
-                    new FileInputStream(new File("src/expl2"))
+                    new FileInputStream(new File( inputFile))
             );
             MLLLexer lexer = new MLLLexer(input);
             MLLParser parser = new MLLParser(new CommonTokenStream(lexer));
